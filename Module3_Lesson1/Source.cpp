504f754b03d504b29c9236a42f5929e98d9360da
айкеновад <@234_11>
2018-06-05 09:24:53 +0600

    -

---
+++ Module3_Lesson1/Source.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+#include<time.h>
+
+void main()
+
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+	printf("task number:\t");
+	scanf("%d", &tn);
+
+	if (tn == 1)
+	{
+
+
+	}
